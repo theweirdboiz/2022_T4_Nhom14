fea574c4971c070f0d5c6aa32acb2b8730c6bcda
light1406 <19130022@st.hcmuaf.edu.vn>
2022-10-23 16:45:52 +0700

    insert code db config dao

--- src/dao/DbConfigDao.java
+++ src/dao/DbConfigDao.java
@@ -1,5 +1,14 @@
 package dao;
 
+import model.DbHosting;
+
 public class DbConfigDao {
 
+	public DbHosting getStaggingHosting() {
+		return null;
+	}
+	
+	public DbHosting getDatawareHouseHosting() {
+		return null;
+	}
 }
