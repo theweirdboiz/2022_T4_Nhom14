10950f6b4e989611aba5eb60be72059bab507211
theweirdboiz <19130227@st.hcmuaf.edu.vn>
2022-10-25 23:43:22 +0700

    add Procedure

---
+++ src/dao/control/Procedure.java
@@ -0,0 +1,8 @@
+package dao;
+
+public interface Procedure {
+    String IS_EXISTED_SOURCE_ID = "{CALL IS_EXISTED_SOURCE_ID(?)}";
+    String START_EXTRACT = "";
+    String FINISH_EXTRACT = "";
+    String  CHECK_DATA_TODAY= "";
+}
